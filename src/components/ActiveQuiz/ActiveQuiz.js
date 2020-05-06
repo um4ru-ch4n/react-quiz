20c44dc9975a8a78c2c42afd75465666a1fe2584
00MRX00 <17515560@mail.ru>
2020-05-06 17:01:47 +0300

    йцук

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './ActiveQuiz.css';
+import AnswersList from './AnswersList/AnswersList';
+
+const ActiveQuiz = props => {
+    return (
+        <div className={classes.ActiveQuiz}>
+            <p className={classes.Question}>
+                <span>
+                    <strong>{props.questionNumber}.</strong>&nbsp;
+                    {props.question}
+                </span>
+
+                <small>{props.questionNumber} из {props.quizLength}</small>
+            </p>
+
+            <AnswersList
+                answers={props.answers}
+                onAnswerClick={props.onAnswerClick}
+                state={props.state}
+            />
+        </div>
+    );
+}
+
+export default ActiveQuiz;
